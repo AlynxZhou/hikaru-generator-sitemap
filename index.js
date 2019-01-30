a440765e50634d070e919834f8e477db7d11ac09
AlynxZhou <alynx.zhou@gmail.com>
2019-01-30 11:22:05 +0800

    Updated for newer api.

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ module.exports = (hikaru) => {
   const {getURLFn, getPathFn} = hikaru.utils
   const {File} = hikaru.types
   hikaru.generator.register('afterProcessing', (site) => {
-    if (!site.get('siteConfig')['sitemap']['enable']) {
+    if (!site['siteConfig']['sitemap']['enable']) {
       return site
     }
     const tmpContent = fs.readFileSync(path.join(__dirname, 'sitemap.njk'), 'utf8')
     const content = nunjucks.renderString(tmpContent, {
-      "posts": site.get("posts"),
-      "getURL": getURLFn(site.get("siteConfig")["baseURL"], site.get("siteConfig")["rootDir"]),
-      "getPath": getPathFn(site.get("siteConfig")["rootDir"])
+      "posts": site["posts"],
+      "getURL": getURLFn(site["siteConfig"]["baseURL"], site["siteConfig"]["rootDir"]),
+      "getPath": getPathFn(site["siteConfig"]["rootDir"])
     })
-    const file = new File(site.get('docDir'))
-    file["docPath"] = site.get("siteConfig")["sitemap"]["path"] || "sitemap.xml"
+    const file = new File(site['siteConfig']['docDir'])
+    file["docPath"] = site["siteConfig"]["sitemap"]["path"] || "sitemap.xml"
     file['content'] = content
     site.put('files', file)
     return site
