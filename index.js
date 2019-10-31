bbee1c6c1c9fb57aa613cad50d38cdc7460b73e7
Alynx Zhou <alynx.zhou@gmail.com>
2019-10-31 13:32:15 +0800

    Updated to use Decorator.

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-const fs = require('fs')
 const path = require('path')
-const nunjucks = require('nunjucks')
 module.exports = (hikaru) => {
-  const {getURLFn, getPathFn} = hikaru.utils
   const {File} = hikaru.types
+  hikaru.decorator.register('sitemap', path.join(__dirname, 'sitemap.njk'))
   hikaru.generator.register('sitemap', (site) => {
     if (!site['siteConfig']['sitemap']['enable']) {
       return
     }
-    const tmpContent = fs.readFileSync(path.join(__dirname, 'sitemap.njk'), 'utf8')
-    const content = nunjucks.renderString(tmpContent, {
-      'posts': site['posts'],
-      'getURL': getURLFn(site['siteConfig']['baseURL'], site['siteConfig']['rootDir']),
-      'getPath': getPathFn(site['siteConfig']['rootDir'])
-    })
     return new File({
       'docDir': site['siteConfig']['docDir'],
       'docPath': site['siteConfig']['sitemap']['path'] || 'sitemap.xml',
-      'content': content
+      'layout': 'sitemap'
     })
   })
 }
