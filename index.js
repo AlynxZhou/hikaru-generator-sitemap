feb91b7f6e840ac95ff6373141f98b0e56ffb56d
Alynx Zhou <alynx.zhou@gmail.com>
2020-08-06 17:53:55 +0800

    Updated to use standardx for coding style checking.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = async (hikaru) => {
-  const {File} = hikaru.types
-  const fn = await hikaru.compiler.compile(path.join(__dirname, 'sitemap.njk'))
-  hikaru.decorator.register('sitemap', fn)
-  hikaru.generator.register('sitemap', (site) => {
-    if (!site['siteConfig']['sitemap']['enable']) {
-      return
+  const {File} = hikaru.types;
+  const fn = await hikaru.compiler.compile(path.join(__dirname, "sitemap.njk"));
+  hikaru.decorator.register("sitemap", fn);
+  hikaru.generator.register("sitemap", (site) => {
+    if (!site["siteConfig"]["sitemap"]["enable"]) {
+      return;
     }
     return new File({
-      'docDir': site['siteConfig']['docDir'],
-      'docPath': site['siteConfig']['sitemap']['path'] || 'sitemap.xml',
-      'layout': 'sitemap'
-    })
-  })
-}
+      "docDir": site["siteConfig"]["docDir"],
+      "docPath": site["siteConfig"]["sitemap"]["path"] || "sitemap.xml",
+      "layout": "sitemap"
+    });
+  });
+};
