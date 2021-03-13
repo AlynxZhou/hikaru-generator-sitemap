fc70d6f9e680b683b36871876a4313834a90752f
Alynx Zhou <alynx.zhou@gmail.com>
2021-03-13 09:39:48 +0800

    Updated to not register if disabled.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const path = require("path");
 
 module.exports = async (hikaru) => {
+  if (!hikaru.site["siteConfig"]["sitemap"]["enable"]) {
+    return;
+  }
   const {File} = hikaru.types;
   const fn = await hikaru.compiler.compile(path.join(__dirname, "sitemap.njk"));
   hikaru.decorator.register("sitemap", fn);
   hikaru.generator.register("sitemap", (site) => {
-    if (!site["siteConfig"]["sitemap"]["enable"]) {
-      return;
-    }
     return new File({
       "docDir": site["siteConfig"]["docDir"],
       "docPath": site["siteConfig"]["sitemap"]["path"] || "sitemap.xml",
