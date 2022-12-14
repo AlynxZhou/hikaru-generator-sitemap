4d22039e1227eb080b6fb49bab96dfdc8a7a38fb
Alynx Zhou <alynx.zhou@gmail.com>
2022-12-14 22:08:38 +0800

    Updated to use ES module.

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const fs = require("fs/promises");
-const path = require("path");
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
 
-module.exports = async (hikaru) => {
+const pluginDir = path.dirname(new URL(import.meta.url).pathname);
+
+const generateSitemap = async (hikaru) => {
   if (!hikaru.site["siteConfig"]["sitemap"]["enable"]) {
     return;
   }
   const {File} = hikaru.types;
-  const filepath = path.join(__dirname, "sitemap.njk");
+  const filepath = path.join(pluginDir, "sitemap.njk");
   const content = await fs.readFile(filepath, "utf8");
   const fn = await hikaru.compiler.compile(filepath, content);
   hikaru.decorator.register("sitemap", fn, {
-    "dirname": __dirname, "pathSep": path.sep
+    "dirname": pluginDir, "pathSep": path.sep
   });
   hikaru.generator.register("sitemap", (site) => {
     return new File({
@@ -20,3 +22,5 @@ module.exports = async (hikaru) => {
     });
   });
 };
+
+export default generateSitemap;
